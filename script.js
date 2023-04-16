7ebb27289a3efdd824a21b405abfd21f44b0bd87
Vaidy05 <92988611+Vaidy05@users.noreply.github.com>
2023-04-16 10:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function nonRepeat(str){
+let obj=new Object();
+for(let i=0;i<str.length;i++){
+	let ch = str.charAt(i);
+	if(obj[ch]>0){
+	  obj[ch]=obj[ch]+1;
+	}
+	else{
+		obj[ch]=1;
+	}
+}
+
+for(let i in obj){
+	if(obj[i]==1)
+		return i;
+}
+}
+let str = prompt("Enter a string"); 
+alert(nonRepeat(str));
